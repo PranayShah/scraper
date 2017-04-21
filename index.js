789a90ff40e66d7f80f149c69be754b5955888c6
Anakha <Pranay Shah>
2017-04-22 00:13:38 +0530

    Shifted open connections tracking logic away from iterator's test condition

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var async = require('async');
 var fs = require('fs');
 var baseUrl = 'https://medium.com';
 var outputFile = 'out.txt';
-
+var flag = 0;
 /**
 * Parses the body to get anchor tags and stores them in an array
 * @param {string} body - The HTML that needs to be parsed.
@@ -19,53 +19,67 @@ var scrapeLinks = function (body) {
 	let $ = cheerio.load (body);
 	let urlsPrevLength = urls.length;
 	$('a').each (function (index, element) {
+		let href = element.attribs['href'];
 		if (href[0] == '/') href = baseUrl + href;
 		if (urls.indexOf (href) == -1) {
 			urls.push (href);
 		}
 	});
-	if (openConnections == 4 || toScrape == urlsPrevLength) {
+	if ((openConnections == 4 && flag ==1) ||toScrape == urlsPrevLength) {
 		wrapper();
 	}
 }
 
 /**
-* Writes all links occurring at a URL to a file
-* @param {string} url - The url to be crawled recursively
+* Fetches web pages
+* @param {function} callback - Passed a single error object to halt iteration or null otherwise
 */
 var getLinks = function (callback) {
-	openConnections +=1;
-	let url = urls[toScrape];
-	if (/https?:\/\/medium.com.*/.test (url)) {
-		request(url, function (error, response, body) {
-			if (error) {
-				return callback (error);
-			}
-			else {
-				scrapeLinks (body); 
-
-			}
-		});
-	}
-	return callback(null);
-}
-var notAllCrawled = function () {
-	return openConnections <5 && ++toScrape < urls.length;
-}
-var done = function (error) {
-	if (error) console.log (error);
-	else {
-		if (true) {
-			fs.writeFile (outputFile, urls.join (','), function (err) {
-				if (err) console.log (err);
+	if (openConnections < 5) {
+		let url = urls[toScrape++];
+		if (/https?:\/\/medium.com.*/.test (url)) {
+			openConnections +=1;
+			request(url, function (error, response, body) {
+				openConnections -= 1;
+				if (error) {
+					console.log(error);
+				}
+				else {
+					scrapeLinks (body);
+				}
 			});
 		}
+		return callback(null);
+	}
+	else {
+		flag=1;
+		return callback({openConnections : 5});
 	}
 }
-var urls = [baseUrl], toScrape = 0, openConnections =1;
+
+/**
+* Test condition for async.whilst
+*/
+var notAllCrawled = function () {
+	return toScrape < urls.length;
+}
+
+/**
+* Final callback called after the test function has failed
+* @param {object} [error]
+*/
+var done = function (error) {
+	if (error && error.openConnections == 5 ) return;
+	else if (openConnections == 0) {
+		fs.writeFile (outputFile, urls.join (','), function (err) {
+			if (err) console.log (err);
+		});
+	}
+}
+
+var urls = [baseUrl], toScrape = 0, openConnections =0;
 
 var wrapper = function () {
-	async.doWhilst (getLinks, notAllCrawled, done);
+	async.whilst (notAllCrawled, getLinks, done);
 }
 wrapper();
-
