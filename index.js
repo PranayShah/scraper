855c378869f69b68ee48279e33bb0b9d69a9442f
Anakha <Pranay Shah>
2017-04-21 17:36:17 +0530

    Using asyn.doWhilst

--- index.js
+++ index.js
@@ -4,57 +4,65 @@
  * Use of ECMAScript6 is minimum.
  */
 
-var request = require('request');
-var cheerio = require('cheerio');
-var fs = require('fs');
-var baseUrl = 'https://medium.com';
-var outputFile = 'out.txt';
+ var request = require('request');
+ var cheerio = require('cheerio');
+ var async = require('async');
+ var fs = require('fs');
+ var baseUrl = 'https://medium.com';
+ var outputFile = 'out.txt';
 
 /**
  * Parses the body to get anchor tags and stores them in an array
  * @param {string} body - The HTML that needs to be parsed.
  */
-var scrapeLinks = function (body) {
-	let $ = cheerio.load (body);
-	$('a').each (function (index, element) {
-		let href = element.attribs['href'];
-		if (href[0] == '/') href = baseUrl + href;
-		if (urls.indexOf (href) == -1 && urls.indexOf (href + '/') == -1) {
-			urls.push (href);
-		}
-	});	
-}
+ var scrapeLinks = function (body) {
+ 	let $ = cheerio.load (body);
+ 	let urlsPrevLength = urls.length;
+ 	$('a').each (function (index, element) {
+ 		if (href[0] == '/') href = baseUrl + href;
+ 		if (urls.indexOf (href) == -1) {
+ 			urls.push (href);
+ 		}
+ 	});
+ 	if (openConnections == 4 || toScrape == urlsPrevLength) {
+ 		wrapper();
+ 	}
+ }
 
  /**
  * Writes all links occurring at a URL to a file
  * @param {string} url - The url to be crawled recursively
  */
-var getLinks = function (url) {
-	request(url, function (error, response, body) {
-		if (error) {
-			console.log (error);
-		}
-		else {
-			openConnections -= 1;
-			scrapeLinks (body);
-			while (openConnections <5 && ++toScrape < urls.length) {
-				openConnections +=1;
-				if (/https?:\/\/medium.com.*/.test (urls[toScrape])) {
-					getLinks (urls[toScrape]);
-				}
-				else {
-					continue;
-				}
-			}
-			if (toScrape == urls.length) {
-				fs.writeFile (outputFile, urls.join (','), function (err) {
-					if (err) console.log (err);
-					else console.log ("Written to ", outputFile, " at ", new Date().toLocaleDateString());
-				});
-			}
-		}
-	});
-}
+ var getLinks = function (callback) {
+ 	openConnections +=1;
+ 	request(urls[toScrape], function (error, response, body) {
+ 		if (error) {
+ 			return callback (error);
+ 		}
+ 		else {
+ 			scrapeLinks (body); 
 
-var urls = [baseUrl], toScrape = 0, openConnections =1;
-getLinks (urls[toScrape]);
+ 		}
+ 	});
+ 	return callback(null);
+ }
+ var notAllCrawled = function () {
+ 	return openConnections <5 && ++toScrape < urls.length;
+ }
+ var done = function (error) {
+ 	if (error) console.log (error);
+ 	else {
+ 		if (true) {
+ 			fs.writeFile (outputFile, urls.join (','), function (err) {
+ 				if (err) console.log (err);
+ 			});
+ 		}
+ 	}
+ }
+ var urls = [baseUrl], toScrape = 0, openConnections =1;
+
+ var wrapper = function () {
+ 	async.doWhilst (getLinks, notAllCrawled, done);
+ }
+ wrapper();
+ 
