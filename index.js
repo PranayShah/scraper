c28f8959b396e873044682667c1406c1bc660140
Anakha <Pranay Shah>
2017-04-21 12:03:56 +0530

    Documenting the API

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
+/**
+ * In the tradeoff between more I/O operations and comsuming more memory, this code chooses the latter. So there is a single point of writing to the disk
+ * Also, this code uses globals liberally. Though not ideal, it makes sense for a small program like this.
+ * Use of ECMAScript6 is minimum.
+ */
+
 var request = require('request');
 var cheerio = require('cheerio');
 var fs = require('fs');
 var baseUrl = 'https://medium.com';
 var outputFile = 'out.txt';
+
+/**
+ * Parses the body to get anchor tags and stores them in an array
+ * @param {string} body - The HTML that needs to be parsed.
+ */
 var scrapeLinks = function (body) {
 	let $ = cheerio.load (body);
 	$('a').each (function (index, element) {
@@ -13,6 +24,11 @@ var scrapeLinks = function (body) {
 		}
 	});	
 }
+
+ /**
+ * Writes all links occurring at a URL to a file
+ * @param {string} url - The url to be crawled recursively
+ */
 var getLinks = function (url) {
 	request(url, function (error, response, body) {
 		if (error) {
@@ -39,5 +55,6 @@ var getLinks = function (url) {
 		}
 	});
 }
+
 var urls = [baseUrl], toScrape = 0, openConnections =1;
 getLinks (urls[toScrape]);
